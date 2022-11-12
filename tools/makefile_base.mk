1b18888cd69eaada8d3f1c45215d308584727548
Roman Stratiienko <r.stratiienko@gmail.com>
2022-11-12 21:18:48 +0200

    Extract common code from makefile to allow building more projects
    Code was reworked significantly. Originally part of the code was copied
    from AOSP, which has Apache 2.0 licence, therefore align all makefiles
    to Apache 2.0
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

---
+++ tools/makefile_base.mk
@@ -0,0 +1,64 @@
+#!/usr/bin/make
+# Variables are replaced with real data by top-level make:
+
+AOSP_ROOT:=$(shell pwd)
+AOSP_OUT_DIR:=$(shell pwd)
+SRC_DIR:=./src
+PATCHES_DIRS:=patches
+LLVM_DIR:=
+
+ifeq ($(AOSPLESS),)
+AOSP_ROOT:=[PLACE_FOR_AOSP_ROOT]
+AOSP_OUT_DIR:=[PLACE_FOR_AOSP_OUT_DIR]
+SRC_DIR:=$(AOSP_ROOT)/[PLACE_FOR_SRC_DIR]
+PATCHES_DIRS:=[PLACE_FOR_PATCHES_DIRS]
+LLVM_DIR:=$(AOSP_ROOT)/[PLACE_FOR_LLVM_DIR]
+endif
+
+COPY_TARGET:=./logs/1.copy.log
+PATCH_TARGET:=./logs/2.patch.log
+
+OUT_BASE_DIR:=$(shell pwd)
+OUT_SRC_DIR:=$(OUT_BASE_DIR)/out_src
+OUT_INSTALL_DIR:=$(OUT_BASE_DIR)/install
+OUT_BUILD_DIR:=$(OUT_BASE_DIR)/build
+OUT_GEN_DIR:=$(OUT_BASE_DIR)/gen
+
+PATCHES:=$(foreach dir,$(PATCHES_DIRS), $(sort $(shell find -L $(AOSP_ROOT)/$(dir)/*.patch -not -path '*/\.*')))
+
+.PHONY: help cleanup all copy patch configure build install src_patch src_unpatch src_gen_pathes
+.DEFAULT_GOAL = help
+SHELL := /bin/bash
+.SHELLFLAGS := -eu -o pipefail -c
+
+help: ## Show this help
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+	@exit || exit # Protection against running this makefile as script (otherwise it will invoke 'rm -rf /*' eventually)
+
+cleanup: ## Cleanup
+	@echo Cleaning up...
+	@[ "$(OUT_SRC_DIR)" ] || ( echo "var is not set"; exit 1 )
+	@[ "$(OUT_INSTALL_DIR)" ] || ( echo "var is not set"; exit 1 )
+	@[ "$(OUT_BUILD_DIR)" ] || ( echo "var is not set"; exit 1 )
+	@shopt -s dotglob nullglob && rm -rf $(OUT_SRC_DIR)/*
+	@shopt -s dotglob nullglob && rm -rf $(OUT_INSTALL_DIR)/*
+	@shopt -s dotglob nullglob && rm -rf $(OUT_BUILD_DIR)/*
+	@rm ./logs/* -f
+
+copy: ## Copy sources into intermediate directory
+copy:
+	@[ "$(OUT_SRC_DIR)" ] || ( echo "var is not set"; exit 1 )
+	@echo Copying...
+	@mkdir -p $(OUT_SRC_DIR)
+	@mkdir -p ./logs
+	@(shopt -s dotglob nullglob && rm -rf $(OUT_SRC_DIR)/*)
+	@(shopt -s dotglob nullglob && rsync -arv $(SRC_DIR)/* $(OUT_SRC_DIR)) 2>&1 > $(COPY_TARGET) || (cat $(COPY_TARGER) && exit 1)
+
+patch: ## Patch sources in intermediate directory
+patch: $(PATCH_TARGET)
+$(PATCH_TARGET): copy
+	@echo Patching...
+	@(cd $(OUT_SRC_DIR) && $(foreach patch,$(PATCHES), echo -e " - Applying $(notdir $(patch))\n" && patch -f -p1 < $(patch) && echo -e "\n" &&) true) 2>&1 > $@.tmp || (cat $@.tmp && exit 1)
+	@mv $@.tmp $@ -f
+
+include project_specific.mk
