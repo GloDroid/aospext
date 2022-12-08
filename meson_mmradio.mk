1765bd48e2d635e3ac20fb594e788fea5fa7f93f
Roman Stratiienko <r.stratiienko@gmail.com>
2022-12-08 21:11:52 +0200

    Add support for mmradio HAL
    https://github.com/GloDroid/mm-radio
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

---
+++ meson_mmradio.mk
@@ -0,0 +1,78 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# AOSPEXT project (https://github.com/GloDroid/aospext)
+#
+# Copyright (C) 2021 GlobalLogic Ukraine
+# Copyright (C) 2021-2022 Roman Stratiienko (r.stratiienko@gmail.com)
+
+AOSPEXT_PROJECT_NAME := MMRADIO
+
+ifneq ($(filter true, $(BOARD_BUILD_AOSPEXT_MMRADIO)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder_ndk \
+    libcutils \
+    liblog \
+    libutils \
+    android.hardware.radio-V1-ndk \
+    android.hardware.radio.config-V1-ndk \
+    android.hardware.radio.data-V1-ndk \
+    android.hardware.radio.messaging-V1-ndk \
+    android.hardware.radio.modem-V1-ndk \
+    android.hardware.radio.network-V1-ndk \
+    android.hardware.radio.sim-V1-ndk \
+    android.hardware.radio.voice-V1-ndk \
+
+MESON_GEN_PKGCONFIGS := \
+    android_base \
+    android_binder_ndk \
+    android_cutils \
+    android_log \
+    android_utils \
+    android.hardware.radio-V1-ndk \
+    android.hardware.radio.config-V1-ndk \
+    android.hardware.radio.data-V1-ndk \
+    android.hardware.radio.messaging-V1-ndk \
+    android.hardware.radio.modem-V1-ndk \
+    android.hardware.radio.network-V1-ndk \
+    android.hardware.radio.sim-V1-ndk \
+    android.hardware.radio.voice-V1-ndk \
+
+LOCAL_SHARED_LIBRARIES += \
+    libglib-2.0 \
+    libgio-2.0 \
+    libgobject-2.0 \
+    libmm-glib \
+
+MESON_GEN_PKGCONFIGS += \
+    glib-2.0:2.75.1 \
+    gmodule-2.0:2.75.1 \
+    gobject-2.0:2.75.1 \
+    mm-glib:1.21.1 \
+
+MESON_BUILD_ARGUMENTS := \
+
+# Format: TYPE:REL_PATH_TO_INSTALL_ARTIFACT:VENDOR_SUBDIR:MODULE_NAME:SYMLINK_SUFFIX
+# TYPE one of: lib, bin, etc
+AOSPEXT_GEN_TARGETS := \
+    bin:hw/android.hardware.mm-radio-service:hw:android.hardware.mm-radio-service: \
+    etc:init/android.hardware.radio.rc:init:android.hardware.mmradio.rc: \
+    etc:vintf/manifest/android.hardware.radio.xml:vintf/manifest:android.hardware.mmradio.xml: \
+    $(BOARD_MMRADIO_EXTRA_TARGETS)
+
+# Build first ARCH only
+LOCAL_MULTILIB := first
+include $(LOCAL_PATH)/meson_cross.mk
+AOSPEXT_TARGETS_DEP:=$(MESON_GEN_FILES_TARGET)
+AOSPEXT_PROJECT_INSTALL_DIR:=$(dir $(AOSPEXT_TARGETS_DEP))/install
+AOSPEXT_PROJECT_OUT_INCLUDE_DIR:=
+include $(LOCAL_PATH)/aospext_gen_targets.mk
+
+#-------------------------------------------------------------------------------
+
+endif # BOARD_BUILD_AOSPEXT_MMRADIO
