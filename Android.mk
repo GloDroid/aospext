9f6bc61e2101e520ecddf3db1133fd7856757ddb
Roman Stratiienko <r.stratiienko@gmail.com>
2023-11-06 02:12:26 +0200

    Add armnn and ComputeLibrary projects
    armnn: https://github.com/ARM-software/armnn
    ComputeLibrary: https://github.com/ARM-software/ComputeLibrary
    
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/cargo_mmradio.mk
 
+include $(LOCAL_PATH)/cmake_armnn.mk
+include $(LOCAL_PATH)/cmake_computelibrary.mk
 include $(LOCAL_PATH)/cmake_flatbuffers.mk
 
 include $(LOCAL_PATH)/ffmpeg.mk
