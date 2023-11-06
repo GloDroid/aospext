ca451b45e06451c4a4625c393e3c8f4330e0ae13
Roman Stratiienko <r.stratiienko@gmail.com>
2023-11-06 02:08:38 +0200

    Sort Android.mk includes alphabetically
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,15 +8,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/cargo_mmradio.mk
 
+include $(LOCAL_PATH)/ffmpeg.mk
+
 include $(LOCAL_PATH)/meson_dav1d.mk
 include $(LOCAL_PATH)/meson_dbus.mk
-include $(LOCAL_PATH)/meson_glib.mk
-include $(LOCAL_PATH)/meson_libgudev.mk
-include $(LOCAL_PATH)/meson_mesa3d.mk
-include $(LOCAL_PATH)/meson_libcamera.mk
-include $(LOCAL_PATH)/meson_libqmi.mk
-include $(LOCAL_PATH)/meson_minigbm.mk
-include $(LOCAL_PATH)/meson_modemmanager.mk
 include $(LOCAL_PATH)/meson_drmhwcomposer.mk
 include $(LOCAL_PATH)/meson_ffmpegcodec2.mk
-include $(LOCAL_PATH)/ffmpeg.mk
+include $(LOCAL_PATH)/meson_glib.mk
+include $(LOCAL_PATH)/meson_libcamera.mk
+include $(LOCAL_PATH)/meson_libgudev.mk
+include $(LOCAL_PATH)/meson_libqmi.mk
+include $(LOCAL_PATH)/meson_mesa3d.mk
+include $(LOCAL_PATH)/meson_minigbm.mk
+include $(LOCAL_PATH)/meson_modemmanager.mk
